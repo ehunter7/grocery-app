12bb8a8aba05c110a322ef08c077e480c1276382
Eric Hunter <ehunter7@live.com>
2021-09-27 21:56:55 -0700

    add ios checkbox

--- client/components/ListItem.jsx
+++ client/components/ListItem.jsx
@@ -1,44 +1,40 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import CheckBox from '@react-native-community/checkbox';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
+// import CheckBox from '@react-native-community/checkbox';
+import { CheckBox } from 'react-native-elements'
+import Swipeable from 'react-native-gesture-handler/Swipeable'
 
-
-
-export default function ListItem({item, renderRightActions, handlePress}) {
-
-
-
-    return (
-        <Swipeable renderRightActions={renderRightActions}>
-        <View style={styles.container}>
+export default function ListItem({ item, renderRightActions, handlePress }) {
+  return (
+    <Swipeable renderRightActions={renderRightActions}>
+      <View style={styles.container}>
         <View>
-
-<CheckBox
-  disabled={false}
-  value={item.checked}
-  onValueChange={(newValue) => {
-    handlePress(item);
- }}
-/>
+          <CheckBox
+            // disabled={false}
+            checked={item.checked}
+            onPress={(newValue) => {
+              handlePress(item)
+            }}
+          />
         </View>
-            <View>
-
-            <Text style={item.checked ? {textDecorationLine: 'line-through'}: null} >{item.name}</Text>
-            </View>
-
+        <View>
+          <Text
+            style={item.checked ? { textDecorationLine: 'line-through' } : null}
+          >
+            {item.name}
+          </Text>
         </View>
-        </Swipeable>
-    )
+      </View>
+    </Swipeable>
+  )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flexDirection: "row",
-        alignItems: "center",
-        marginTop: 5,
-        marginLeft: 30,
-        flexWrap: "wrap",
-    },
-
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+    marginLeft: 30,
+    flexWrap: 'wrap',
+  },
 })
